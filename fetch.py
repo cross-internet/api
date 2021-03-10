597bf91d8c1f83e2da3f891ee8a47726764d88e6
Diamond Cat <uyuuki0919@icloud.com>
2021-03-10 17:33:58 +0900

    add save

--- fetch.py
+++ fetch.py
@@ -1,5 +1,6 @@
 import random
 from datetime import datetime
+from pathlib import Path
 from urllib.parse import quote_plus
 
 import pandas as pd
@@ -7,6 +8,14 @@ import requests
 
 HIRA_KATA = [chr(i) for i in [*range(0x3040, 0x309F + 1), *range(0x30A0, 0x30FF + 1)]]
 HEADERS = {"User-Agent": "Mozilla/5.0"}
+
+
+def update_csv(path, df, after_concat):
+    _path = Path(path)
+    _df = pd.read_csv(_path) if _path.is_file() else pd.DataFrame()
+    _df = pd.concat([df, _df])
+    _df = after_concat(_df)
+    _df.to_csv(path, index=False)
 
 
 def fetch_invidious_domains():
@@ -49,8 +58,10 @@ def fetch_invidious_videos(search_list, domain):
 invidious_domains = fetch_invidious_domains()
 print(invidious_domains)
 
-invidious_videos = fetch_invidious_videos(random.choices(HIRA_KATA, k=int(len(HIRA_KATA) / 4)), invidious_domains.name[0])
+invidious_videos = fetch_invidious_videos(random.choices(HIRA_KATA, k=int(len(HIRA_KATA) / 4)), "invidious.snopyta.org")
 print(invidious_videos)
+
+update_csv("public/invidious_videos.csv", invidious_videos, lambda df: df.drop_duplicates(subset=["id"]).sort_values(by=["views"], ascending=False))
 
 for _, video in invidious_videos.iterrows():
     print(f"https://www.youtube.com/watch?v={video.id} ({video.views} views)")
